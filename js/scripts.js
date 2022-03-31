29bb42f5f972ed2719e032d077a21a942bbc81d4
jiyeon524 <102651767+jiyeon524@users.noreply.github.com>
2022-03-31 11:35:35 +0900

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,34 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+const myForm = document.querySelector('#businfo');
+myForm.addEventListener('start-button', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  const url = 'https://rzgz7n5oq5.execute-api.ap-northeast-2.amazonaws.com/dev';
+
+  const file = {
+    station_id: 5,
+    bus_num: 360
+  };
+
+  var formData = new FormData();
+  formData.append("upload_file" , file);
+  
+  var xmlHttp = new XMLHttpRequest();
+
+  xmlHttp.open("GET", url, true);     
+  
+  xmlHttp.setRequestHeader('Content-Type', 'application/json');
+  
+  xmlHttp.onreadystatechange = function() {
+    if (xmlHttp.readyState == XMLHttpRequest.DONE && xmlHttp.status == 200 ) {
+      console.log(xmlHttp.responseText);
+    }
+  };
+  
+  xmlHttp.send(formData); 
+  console.log(xmlHttp.responseText); 
+}
